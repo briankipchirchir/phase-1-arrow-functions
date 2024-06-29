219b6f75787a405066d5ac17610b2f049868004f
briankipchirchir <kbrianngetich@gmail.com>
2024-06-29 12:55:15 +0300

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide= (a,b)=>{
+    console.log(a/b);
+    return a/b;
+}
+    
+divide(2000,100);
+
+//arrow function for square//
+
+const square=(x)=>{
+    console.log(x**2);
+    return x**2;
+}
+square(2);
+
+//add function//
+const add=(a,b)=>{
+    console.log(a+b);
+    return a+b;
+}
+add(3,4);
